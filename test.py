c2e98275f1a7b3d90fb9045eec97c83549d63138
Andrew Chen <1062446749@qq.com>
2022-05-14 13:21:34 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from data.dataUtils import write_result
 from data.data import get_test_loader, get_loader
 
 
-def test(loader, id2label, model_path = 'model.pth'):
+def test(loader, id2label, model_path='model.pth'):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = Model().to(device)
     model.load_state_dict(torch.load(model_path, map_location=device))
@@ -18,13 +18,13 @@ def test(loader, id2label, model_path = 'model.pth'):
         for x, name in tqdm(loader):
             x = x.to(device)
             y = model(x)  # N, D
-            _, y = torch.max(y, dim = 1) # (N,)
+            _, y = torch.max(y, dim=1)  # (N,)
 
             for i, name in enumerate(list(name)):
-                result[name] = id2label[y[i].item()]
-
+                result[name] = y[i].item()
 
     write_result(result)
+
 
 if __name__ == '__main__':
     loader, id2label = get_test_loader()
@@ -32,5 +32,4 @@ if __name__ == '__main__':
     #                                         train_image_path='./data/public_dg_0416/train/',
     #                                         valid_image_path='./data/public_dg_0416/train/',
     #                                         label2id_path='./data/dg_label_id_mapping.json')
-    test(loader, id2label,model_path='resnet18.pth')
-
+    test(loader, id2label, model_path='resnet18.pth')
